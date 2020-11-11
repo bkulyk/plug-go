14dbe0509f4c3c7f5cef0a50728537a4c8eef841
Brian Kulyk <brian@kulyk.ca>
2020-11-11 12:13:43 -0800

    fix tabs, madness

--- server.go
+++ server.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"github.com/namsral/flag"
-	"github.com/stianeikeland/go-rpio"
-	"net/http"
+  "fmt"
+  "log"
+  "github.com/namsral/flag"
+  "github.com/stianeikeland/go-rpio"
+  "net/http"
   "os"
   "time"
 )
 
 var (
-	port uint
+  port uint
 
-	pinMap = map[string]rpio.Pin{
-		"19": rpio.Pin(19),
-		"26": rpio.Pin(26),
-	}
+  pinMap = map[string]rpio.Pin{
+    "19": rpio.Pin(19),
+    "26": rpio.Pin(26),
+  }
 )
 
 func toggleHandler(w http.ResponseWriter, r *http.Request) {
-	pins, ok := r.URL.Query()["pin"]
+  pins, ok := r.URL.Query()["pin"]
 
-	if !ok || len(pins[0]) < 1 {
-		log.Println("Url Param 'pin' is missing")
-		return
-	}
+  if !ok || len(pins[0]) < 1 {
+    log.Println("Url Param 'pin' is missing")
+    return
+  }
 
-	pin := string(pins[0])
+  pin := string(pins[0])
 
-	toggle(pin)
+  toggle(pin)
 }
 
 func readAndValidateConfig() {
-	flag.UintVar(&port, "port", 8080, "Port to serve api on")
-	flag.Parse()
+  flag.UintVar(&port, "port", 8080, "Port to serve api on")
+  flag.Parse()
 }
 
 func toggle(pin string) {
-	pinMap[pin].High()
-	time.Sleep(time.Second)
-	pinMap[pin].Low()
+  pinMap[pin].High()
+  time.Sleep(time.Second)
+  pinMap[pin].Low()
 }
 
 func main() {
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer rpio.Close()
+  if err := rpio.Open(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  defer rpio.Close()
 
-	for key, pin := range pinMap {
-		fmt.Printf("Set pin for output %s\n", key)
-		pin.Output()
-	}
+  for key, pin := range pinMap {
+    fmt.Printf("Set pin for output %s\n", key)
+    pin.Output()
+  }
 
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/toggle", toggleHandler)
+  fileServer := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fileServer)
+  http.HandleFunc("/toggle", toggleHandler)
 
-	readAndValidateConfig()
+  readAndValidateConfig()
 
-	portNumber := fmt.Sprintf(":%d", port)
-	fmt.Printf("Starting server at port %s\n", portNumber)
-	if err := http.ListenAndServe(portNumber, nil); err != nil {
-		log.Fatal(err)
-	}
+  portNumber := fmt.Sprintf(":%d", port)
+  fmt.Printf("Starting server at port %s\n", portNumber)
+  if err := http.ListenAndServe(portNumber, nil); err != nil {
+    log.Fatal(err)
+  }
 }
