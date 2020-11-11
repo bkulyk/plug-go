36980a7ecbeecf72c1f4c336664e83b08fda22ec
Brian Kulyk <brian@kulyk.ca>
2020-11-11 11:53:03 -0800

    was exploding when initing the pins in another func, move to main, add func to toggle the pin instead of duplication

--- server.go
+++ server.go
@@ -8,8 +8,17 @@ import (
 	"github.com/namsral/flag"
 	"github.com/stianeikeland/go-rpio"
 	"net/http"
-  // "os"
+  "os"
   "time"
+)
+
+var (
+	port uint
+
+	pinMap = map[string]rpio.Pin{
+		"19": rpio.Pin(19),
+		"26": rpio.Pin(26),
+	}
 )
 
 func toggleHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,26 +31,7 @@ func toggleHandler(w http.ResponseWriter, r *http.Request) {
 
 	pin := string(pins[0])
 
-  log.Println("Url Param 'pin' is: " + pin)
-
-  pinMap[pin].High()
-  time.Sleep(time.Second)
-  pinMap[pin].Low()
-}
-
-var pinMap map[string]rpio.Pin
-var port uint
-
-func initPins() {
-  if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-  defer rpio.Close()
-
-  pinMap = make(map[string]rpio.Pin)
-	pinMap["26"] = rpio.Pin(26)
-	pinMap["19"] = rpio.Pin(19)
+	toggle(pin)
 }
 
 func readAndValidateConfig() {
@@ -49,13 +39,29 @@ func readAndValidateConfig() {
 	flag.Parse()
 }
 
+func toggle(pin string) {
+	pinMap[pin].High()
+	time.Sleep(time.Second)
+	pinMap[pin].Low()
+}
+
 func main() {
-	initPins()
-	readAndValidateConfig()
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	for key, pin := range pinMap {
+		fmt.Printf("Set pin for output %s\n", key)
+		pin.Output()
+	}
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/toggle", toggleHandler)
+
+	readAndValidateConfig()
 
 	portNumber := fmt.Sprintf(":%d", port)
 	fmt.Printf("Starting server at port %s\n", portNumber)
