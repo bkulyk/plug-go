4c2bbdac1d2126596df54d8768f6875f18f3c90e
Brian Kulyk <brian@kulyk.ca>
2020-11-11 13:50:02 -0800

    use echo microframework, a little nicer to look at

--- server.go
+++ server.go
@@ -1,69 +1,61 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "github.com/namsral/flag"
-  "github.com/stianeikeland/go-rpio"
-  "net/http"
-  "os"
-  "time"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/namsral/flag"
+	"github.com/stianeikeland/go-rpio"
 )
 
 var (
-  port uint
+	port uint
 
-  pinMap = map[string]rpio.Pin{
-    "19": rpio.Pin(19),
-    "26": rpio.Pin(26),
-  }
+	pinMap = map[string]rpio.Pin{
+		"19": rpio.Pin(19),
+		"26": rpio.Pin(26),
+	}
 )
 
-func toggleHandler(w http.ResponseWriter, r *http.Request) {
-  pins, ok := r.URL.Query()["pin"]
-
-  if !ok || len(pins[0]) < 1 {
-    log.Println("Url Param 'pin' is missing")
-    return
-  }
-
-  pin := string(pins[0])
-
-  toggle(pin)
-}
-
 func readAndValidateConfig() {
-  flag.UintVar(&port, "port", 8080, "Port to serve api on")
-  flag.Parse()
+	flag.UintVar(&port, "port", 8080, "Port to serve api on")
+	flag.Parse()
 }
 
-func toggle(pin string) {
-  pinMap[pin].High()
-  time.Sleep(time.Second)
-  pinMap[pin].Low()
+func togglePin(pin string) {
+	pinMap[pin].High()
+	time.Sleep(time.Second)
+	pinMap[pin].Low()
 }
 
 func main() {
-  if err := rpio.Open(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  defer rpio.Close()
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
 
-  for key, pin := range pinMap {
-    fmt.Printf("Set pin for output %s\n", key)
-    pin.Output()
-  }
+	for key, pin := range pinMap {
+		fmt.Printf("Set pin for output %s\n", key)
+		pin.Output()
+	}
 
-  fileServer := http.FileServer(http.Dir("./static"))
-  http.Handle("/", fileServer)
-  http.HandleFunc("/toggle", toggleHandler)
+	readAndValidateConfig()
 
-  readAndValidateConfig()
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Recover())
+	e.GET("/toggle", toggleHandler)
+	e.Static("/", "static")
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+}
 
-  portNumber := fmt.Sprintf(":%d", port)
-  fmt.Printf("Starting server at port %s\n", portNumber)
-  if err := http.ListenAndServe(portNumber, nil); err != nil {
-    log.Fatal(err)
-  }
+func toggleHandler(c echo.Context) error {
+	pin := c.QueryParam("pin")
+	togglePin(pin)
+	return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("/?pin=%s", pin))
 }
